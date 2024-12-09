d1fe9f7933b195ea69d7b80259a92fd3fcd4c66b
arthur <tehtaq@gmail.com>
2024-12-09 22:39:08 +0100

    d4

--- src/day4/main.py
+++ src/day4/main.py
@@ -33,16 +33,16 @@ def p1():
                 if "".join([a[x] for a in inp[y:y+4]]) == WORD:
                     sum += 1
             # Vertical up
-            if y-3 >= 0:
-                if "".join([a[x] for a in inp[y:y-4:-1]]) == WORD:
+            if y+3 <= YMAX:
+                if "".join([a[x] for a in inp[y:y+4][::-1]]) == WORD:
                     sum += 1
             # Horizontal right
             if x+3 <= XMAX:
                 if inp[y][x:x+4] == WORD:
                     sum += 1
             # Horizontal left
-            if x-3 >= 0:
-                if inp[y][x:x-4:-1] == WORD:
+            if x+3 <= XMAX:
+                if inp[y][x:x+4][::-1] == WORD:
                     sum += 1
             # Diag ydown xright
             if x+3 <= XMAX and y+3 <= YMAX:
@@ -63,16 +63,31 @@ def p1():
             if fsum != sum:
                 used[y][x] = inp[y][x]
 
-
+    print(inp[121][0:0+4][::-1])
     print(np.matrix(used))
     print(tsum)
 
     return sum
 
 def p2():
-    inp = inp_lines()
+    inp = [x.strip("\n") for x in inp_lines()]
 
-    return 42
+    XMAX = len(inp[0]) - 1
+    YMAX = len(inp) - 1
+    WORD = "MAS"
+
+    match = 0
+    for y in range(len(inp)):
+        for x in range(len(inp[0])):
+            cur = inp[y][x]
+            if cur == 'A' and y > 0 and x > 0 and x < XMAX and y < YMAX:
+                t1 = inp[y-1][x-1] + cur + inp[y+1][x+1]
+                t2 = inp[y-1][x+1] + cur + inp[y+1][x-1]
+                if (t1 == WORD or t1 == WORD[::-1]) and (t2 == WORD or t2 == WORD[::-1]):
+                    match += 1
+
+
+    return match
 
 def inp_lines():
     return open(input, 'r').readlines();
