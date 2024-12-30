74ab8347c31cd774b298b260ae5fd512c7bc1d7a
arthur <tehtaq@gmail.com>
2024-12-30 01:57:04 +0100

    d12p1

--- src/day12/main.py
+++ src/day12/main.py
@@ -5,38 +5,32 @@ import numpy as np
 import copy
 import itertools
 
-input = f"{pathlib.Path(__file__).parent.resolve()}/tinput.txt"
+input = f"{pathlib.Path(__file__).parent.resolve()}/input.txt"
 
 def p1():
     inp = [(list(x.strip("\n"))) for x in inp_lines()]
-    print(np.matrix(inp))
 
     regions = dict()
     for x in range(len(inp[0])):
         for y in range(len(inp)):
-            skip = False
-            # if inp[y][x] in regions and any((x, y) in x for x in regions[inp[y][x]]):
-            #     continue
-            if inp[y][x] in regions:
-                for regs in regions[inp[y][x]]:
-                    if (x, y) in regs:
-                        skip = True
-                        break
-            if skip:
+            if inp[y][x] in regions and any((x, y) in regs for regs in regions[inp[y][x]]):
                 continue
 
             reg = list()
-            reg = find_region1(inp, x, y, reg)
+            reg = find_region(inp, x, y, reg)
             if inp[y][x] not in regions:
                 regions[inp[y][x]] = list()
             if reg not in regions[inp[y][x]]:
                 regions[inp[y][x]].append(reg)
 
+    res = 0
     for k, v in regions.items():
         for reg in v:
-            print(f"{k}: {len(reg)}")
-    print(regions)
-    return 42
+            area = len(reg)
+            perimeter = find_peri(inp, reg)
+            res += (area * perimeter)
+
+    return res
 
 def p2():
     inp = [(list(x.strip("\n"))) for x in inp_lines()]
@@ -45,36 +39,7 @@ def p2():
 
 dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 rc = 0
-# def find_region(grid, x, y, regs=dict(), reg=list(), done=set()):
-#     if (x, y) in done:
-#         return regs
-#
-#     # TODO: Make reg into set
-#     if grid[y][x] not in regs:
-#         regs[grid[y][x]] = list()
-#
-#     if (x, y) not in regs[grid[y][x]]:  # Check if we already handled this pos
-#         reg.append((x, y))
-#         done.add((x, y))
-#
-#     for dir in dirs:
-#         dx, dy = x + dir[0], y + dir[1]
-#         if dx >= len(grid[0]) or dx < 0 or dy >= len(grid) or dy < 0:
-#             continue
-#         if grid[dy][dx] != grid[y][x]:
-#             continue
-#
-#         # regs[grid[dy][dx]].append((dx, dy))
-#         # reg.append((dx, dy))
-#         find_region(grid, dx, dy, regs, reg)
-#
-#     if reg not in regs[grid[y][x]]:
-#         regs[grid[y][x]].append(copy.deepcopy(reg))
-#         reg.clear()
-#
-#     return regs
-
-def find_region1(grid, x, y, reg):
+def find_region(grid, x, y, reg):
     if (x, y) in reg:  # Check if we already handled this pos
         return reg
 
@@ -87,9 +52,23 @@ def find_region1(grid, x, y, reg):
         if grid[dy][dx] != grid[y][x]:
             continue
 
-        find_region1(grid, dx, dy, reg)
+        find_region(grid, dx, dy, reg)
 
     return reg
+
+def find_peri(grid, region):
+    perimeter = 0
+    for x, y in region:
+        for dir in dirs:
+            dx, dy = x + dir[0], y + dir[1]
+            if dx >= len(grid[0]) or dx < 0 or dy >= len(grid) or dy < 0:
+                perimeter += 1
+                continue
+            if grid[y][x] != grid[dy][dx]:
+                perimeter += 1
+                continue
+
+    return perimeter
 
 def inp_lines():
     return open(input, 'r').readlines()
